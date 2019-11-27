a2b3cad4e9acfdb7c4c68a2503d774af96eb4569
Douglas Hammarstam <dogge20@hotmail.com>
2019-11-27 04:19:03 +0100

    Initial commit

---
+++ part1.js
@@ -0,0 +1,49 @@
+
+var myObject = {
+    create: function (protList) {
+        var instance = {
+            protList: [],
+
+            call: function (funcName, parameters) {
+                //Check if this object has the method
+                if(this.hasOwnProperty(funcName)){
+                    console.log(typeof parameters);
+                    console.log(this[funcName].toString());
+                    var func1 = this[funcName];
+                    console.log(func1.apply(this, parameters));
+                    return this[funcName].apply(this, parameters);
+
+                } else{
+                    //check other cases ie , all the objects in the list protlist
+                    for (i = 0; i< protList.length; i++){
+                        if(protList[i].call(funcName, parameters) != null){
+                            return protList[i].call(funcName, parameters);
+                        } else{
+
+                        }
+
+                        console.log("looping through other objects");
+                    }
+                }
+                copy.prototype = myObject;
+
+
+            }
+
+        };
+        return instance;
+
+    }
+}
+
+
+//Test code
+
+var obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+/*var obj1 = myObject.create([obj0]);
+var obj2 = myObject.create([]);
+obj2.func = function(arg) { return "func2: " + arg; };
+var obj3 = myObject.create([obj1, obj2]);
+var result = obj3.call("func", "hello") ;*/
+console.log(obj0.call("func", "hello"))
