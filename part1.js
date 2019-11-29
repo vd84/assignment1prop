66b1a107dd815a46a0754336eeaadbb31822f7b5
Douglas Hammarstam <dogge20@hotmail.com>
2019-11-29 04:03:22 +0100

    All tests pass

--- part1.js
+++ part1.js
@@ -1,53 +1,59 @@
 var myObject = {
-    create: function (protList) {
+    create: function (protListParameter) {
         var instance = {
-            protList: [],
+            protList: protListParameter,
             call: function (funcName, parameters) {
                 //Check if this object has the method
                 if (this.hasOwnProperty(funcName)) {
                     return this[funcName](parameters);
                 } else {
-                    if (protList != null) {
-                        for (i = 0; i < protList.length; i++) {
-                            if (protList[i].call(funcName, parameters) != null) {
-                                return protList[i].call(funcName, parameters);
+                    if (this.protList.length !== null) {
+                        for (let i = 0; i < this.protList.length; i++) {
+                            if (this.protList[i].call(funcName, parameters) !== undefined) {
+                                return this.protList[i].call(funcName, parameters);
                             }
                         }
                     }
 
                 }
 
+
             }
-        }
+
+
+        };
         return instance;
     }
 
-}
+
+};
 
 
 //Test code
+
 var obj0 = myObject.create(null);
 obj0.func = function (arg) {
     return "func0: " + arg;
 };
 var obj1 = myObject.create([obj0]);
 var obj2 = myObject.create([]);
-obj2.func = function (arg) {
-    return "func2: " + arg;
-};
+
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
 
-obj0 = myObject.create(null);
+var obj0 = myObject.create(null);
 obj0.func = function (arg) {
     return "func0: " + arg;
 };
-obj1 = myObject.create([obj0]);
-obj2 = myObject.create([]);
-obj3 = myObject.create([obj2, obj1]);
-result = obj3.call("test", ["hello"]);
+var obj1 = myObject.create([obj0]);
+var obj2 = myObject.create([]);
+
+var obj3 = myObject.create([obj2, obj1]);
+//obj0.addProtoType(obj1);
+var result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
+
 
 obj0 = myObject.create(null);
 obj0.func = function (arg) {
@@ -55,3 +61,4 @@ obj0.func = function (arg) {
 };
 result = obj0.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
+
