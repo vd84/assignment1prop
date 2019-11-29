a4e09da686f996dbf22c367e0c0d641e0d0bbdad
Douglas Hammarstam <dogge20@hotmail.com>
2019-11-29 07:04:32 +0100

    part 2 start

--- part2.js
+++ part2.js
@@ -1,50 +1,78 @@
-createClass = function (className, superClassList) {
+createClass = function (className, superClassListParameter) {
 
-    var object = {
+    let object = {
         name: className,
-        superClasses: superClassList,
+        superClassList: [],
         new: function () {
-
-
-
-            var instance = {
+            let instance = {
                 call: function (funcName, parameters) {
-                    if(this.hasOwnProperty(funcName)){
+                    console.log("fdsfds")
+                    //Check if this object has the method
+                    if (this.hasOwnProperty(funcName)) {
                         return this[funcName](parameters);
-
-
                     } else {
                         //Check if parents has method
-                        if (this.superClasses.length >0 ) {
-                            for (let i = 0; i < this.superClasses.length; i++) {
-                                if (this.superClasses[i].call(funcName, parameters) !== undefined) {
-                                    return this.superClasses[i].call(funcName, parameters);
+                        if (this.superClassList.length > 0) {
+                            for (let i = 0; i < this.superClassList.length; i++) {
+                                if (this.superClassList[i].call(funcName, parameters) !== undefined) {
+                                    return this.superClassList[i].call(funcName, parameters);
                                 }
                             }
-                        };
-                        return instance;
+                        }
+                    }
+                },
+                //Cannot add prototype that introduces circular inheritance
+                addSuperClass: function (object) {
+                    if (object.superClassList !== null) {
+                        for (i = 0; i < object.superClassList.length; i++) {
+                            console.log("in for loop")
+
+                            if (object.superClassList.indexOf(this) !== -1) {
+                                throw new DOMException("Cant use circular inheritence");
+                            } else {
+                                object.superClassList.push(superClassListParameter[i]);
+
+
+                            }
+                        }
 
                     }
-
-                    
                 }
+
             };
+
+            if (superClassListParameter !== null) {
+                for (i = 0; i < superClassListParameter.length; i++) {
+                    if (superClassListParameter[i].superClassList.indexOf(instance) !== -1) {
+                        throw new DOMException("Cant use circular inheritence");
+                    } else {
+                        object.superClassList.push(superClassListParameter[i]);
+                    }
+
+                }
+            }
+            return instance;
+            //Add parents to "prototypeList" if they dont introduce circular inheritance
 
 
         }
-
-
-
     }
-    
+    return object;
+
 };
 
 // TEST
 var class0 = createClass("Class0", null);
-class0.func0 = function(arg) { return "func0: " + arg; };
+class0.func0 = function (arg) {
+    return "func0: " + arg;
+};
 var class1 = createClass("Class1", [class0]);
 var class2 = createClass("Class2", []);
-class2.func2 = function(arg) { return "func2: " + arg; };
+class2.func2 = function (arg) {
+    return "func2: " + arg;
+};
 var class3 = createClass("Class3", [class1, class2]);
 var obj3 = class3.new();
 var result = obj3.call("func2", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
+
